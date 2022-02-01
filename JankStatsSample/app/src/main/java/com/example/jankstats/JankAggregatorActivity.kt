fda2cd0bdbb07df33b5084bc05b49531c7cd8a11
Tomáš Mlynarič <mlykotom@google.com>
2022-02-01 14:57:45 +0100

    refactor: remove unused menu

--- JankStatsSample/app/src/main/java/com/example/jankstats/JankAggregatorActivity.kt
+++ JankStatsSample/app/src/main/java/com/example/jankstats/JankAggregatorActivity.kt
@@ -18,8 +18,6 @@ package com.example.jankstats
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.metrics.performance.PerformanceMetricsState
 import androidx.navigation.NavController
@@ -86,23 +84,7 @@ class JankAggregatorActivity : AppCompatActivity() {
         jankStatsAggregator.issueJankReport("Activity paused")
         jankStatsAggregator.jankStats.isTrackingEnabled = false
     }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
+    
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
--- JankStatsSample/app/src/main/java/com/example/jankstats/MainActivity.kt
+++ JankStatsSample/app/src/main/java/com/example/jankstats/MainActivity.kt
@@ -18,8 +18,6 @@ package com.example.jankstats
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.metrics.performance.JankStats
 import androidx.metrics.performance.PerformanceMetricsState
@@ -92,22 +90,6 @@ class MainActivity : AppCompatActivity() {
         jankStats.isTrackingEnabled = false
     }
     // [END tracking_enabled]
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
