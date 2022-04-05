aea3ca5bc71400edf028ffe1e969f456ce299766
Tomáš Mlynarič <mlykotom@google.com>
2022-04-05 14:47:31 +0200

    feat: add compose sample for adding state

---
+++ JankStatsSample/app/src/main/java/com/example/jankstats/compose/rememberMetricsStateHolder.kt
@@ -0,0 +1,15 @@
+package com.example.jankstats.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalView
+import androidx.metrics.performance.PerformanceMetricsState
+
+/**
+ * Retrieve MetricsStateHolder from compose and remember until the current view changes.
+ */
+@Composable
+fun rememberMetricsStateHolder(): PerformanceMetricsState.MetricsStateHolder {
+    val view = LocalView.current
+    return remember(view) { PerformanceMetricsState.getForHierarchy(view) }
+}
