60d7e21702b090f36188f6fe5ffa8064ea6c58ee
Ben Weiss <benweiss@google.com>
2022-11-18 12:35:04 +0000

    Move preference keys

--- MacrobenchmarkSample/app/src/main/java/com/example/macrobenchmark/target/util/Utils.kt
+++ MacrobenchmarkSample/app/src/main/java/com/example/macrobenchmark/target/util/Utils.kt
@@ -20,9 +20,13 @@ import android.content.Context
 import android.util.TypedValue
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 
 const val TAG = "Perf Sample"
+
+val KEY_USER = stringPreferencesKey("user")
+val KEY_PASSWORD = stringPreferencesKey("password")
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
