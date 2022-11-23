5cf6eb3e3986f75c2cdf8b3c5ae5e59162a0d2b1
Ben Weiss <benweiss@google.com>
2022-11-23 18:04:22 +0000

    Use Compose in MainActivity

--- MacrobenchmarkSample/app/src/main/java/com/example/macrobenchmark/target/activity/MainActivity.kt
+++ MacrobenchmarkSample/app/src/main/java/com/example/macrobenchmark/target/activity/MainActivity.kt
@@ -19,7 +19,20 @@ package com.example.macrobenchmark.target.activity
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.example.macrobenchmark.target.activity.clicklatency.ComposeActivity
 import com.example.macrobenchmark.target.activity.clicklatency.ListViewActivity
 import com.example.macrobenchmark.target.activity.clicklatency.NestedRecyclerActivity
@@ -27,50 +40,70 @@ import com.example.macrobenchmark.target.activity.clicklatency.NonExportedRecycl
 import com.example.macrobenchmark.target.activity.clicklatency.ScrollViewActivity
 import com.example.macrobenchmark.target.activity.clicklatency.USE_RECYCLER_POOLS
 import com.example.macrobenchmark.target.activity.login.LoginActivity
-import com.example.macrobenchmark.target.databinding.ActivityMainBinding
 import com.example.macrobenchmark.target.util.ClickTrace
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.launchLoginActivity.setOnClickListener {
-            launchActivityWithTrace<LoginActivity>()
+        setContent {
+            Column {
+                TopAppBar(title = { Text(text = "Benchmark Sample Target App") })
+                ActivityList()
+            }
         }
+    }
 
-        binding.launchFullyDrawnActivity.setOnClickListener {
-            launchActivityWithTrace<FullyDrawnStartupActivity>()
-        }
 
-        binding.launchRecyclerActivity.setOnClickListener {
-            launchActivityWithTrace<NonExportedRecyclerActivity>()
-        }
-
-        binding.launchListViewActivity.setOnClickListener {
-            launchActivityWithTrace<ListViewActivity>()
-        }
-
-        binding.launchScrollViewActivity.setOnClickListener {
-            launchActivityWithTrace<ScrollViewActivity>()
-        }
-
-        binding.launchComposeList.setOnClickListener {
-            launchActivityWithTrace<ComposeActivity>()
-        }
-
-        binding.nestedRecyclerActivity.setOnClickListener {
-            launchActivityWithTrace<NestedRecyclerActivity>()
-        }
-
-        binding.nestedRecyclerWithPoolsActivity.setOnClickListener {
-            launchActivityWithTrace<NestedRecyclerActivity>(
-                Intent().putExtra(
-                    USE_RECYCLER_POOLS, true
+    @Composable
+    fun ActivityList() {
+        Text(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            text = "This app runs macrobenchmark tests",
+            textAlign = TextAlign.Center
+        )
+        Column {
+            BenchmarkActivityButton(name = "Login") {
+                launchActivityWithTrace<LoginActivity>()
+            }
+            BenchmarkActivityButton(name = "ListView") {
+                launchActivityWithTrace<ListViewActivity>()
+            }
+            BenchmarkActivityButton(name = "Compose") {
+                launchActivityWithTrace<ComposeActivity>()
+            }
+            BenchmarkActivityButton(name = "ScrollView") {
+                launchActivityWithTrace<ScrollViewActivity>()
+            }
+            BenchmarkActivityButton(name = "Fully Drawn") {
+                launchActivityWithTrace<FullyDrawnStartupActivity>()
+            }
+            BenchmarkActivityButton(name = "RecyclerView") {
+                launchActivityWithTrace<NonExportedRecyclerActivity>()
+            }
+            BenchmarkActivityButton(name = "Nested RecyclerView") {
+                launchActivityWithTrace<NestedRecyclerActivity>()
+            }
+            BenchmarkActivityButton(name = "Nested RecyclerView with Pools") {
+                launchActivityWithTrace<NestedRecyclerActivity>(
+                    Intent().putExtra(
+                        USE_RECYCLER_POOLS, true
+                    )
                 )
-            )
+            }
+        }
+    }
+
+    @Composable
+    fun BenchmarkActivityButton(name: String, onClick: () -> Unit) {
+        TextButton(
+            modifier = Modifier.padding(8.dp),
+            onClick = onClick,
+            border = BorderStroke(1.dp, MaterialTheme.colors.primary)
+        ) {
+            Text(name)
         }
     }
 
--- MacrobenchmarkSample/macrobenchmark/src/main/java/com/example/macrobenchmark/baselineprofile/BaselineProfileGenerator.kt
+++ MacrobenchmarkSample/macrobenchmark/src/main/java/com/example/macrobenchmark/baselineprofile/BaselineProfileGenerator.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.macrobenchmark.baselineprofile
 
 import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
@@ -46,7 +62,7 @@ class BaselineProfileGenerator {
 
             with(device) {
                 // Scrolling RecyclerView journey
-                findObject(By.text("RECYCLERVIEW")).clickAndWait(Until.newWindow(), 1_000)
+                findObject(By.text("RecyclerView")).clickAndWait(Until.newWindow(), 1_000)
                 findObject(By.res(packageName, "recycler")).also {
                     it.fling(Direction.DOWN)
                     it.fling(Direction.UP)
@@ -54,7 +70,7 @@ class BaselineProfileGenerator {
                 pressBack()
 
                 // Scrolling LazyColumn journey
-                findObject(By.text("COMPOSE LAZYLIST")).clickAndWait(Until.newWindow(), 1_000)
+                findObject(By.text("Compose")).clickAndWait(Until.newWindow(), 1_000)
                 findObject(By.res("myLazyColumn")).also {
                     it.fling(Direction.DOWN)
                     it.fling(Direction.UP)
