8fc8cc563fff314bc080b128785e3587829a8da8
Ben Weiss <benweiss@google.com>
2023-01-31 11:41:09 +0000

    Upgrade versions to next release cycle

--- MacrobenchmarkSample/app/build.gradle.kts
+++ MacrobenchmarkSample/app/build.gradle.kts
@@ -62,11 +62,11 @@ android {
     // [END macrobenchmark_setup_app_build_type]
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
         freeCompilerArgs = freeCompilerArgs + listOf(
             "-opt-in=kotlin.RequiresOptIn"
         )
