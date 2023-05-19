5d4633f7d8b866b47fe8eb537fe02a45f9ff2365
Tomáš Mlynarič <mlykotom@google.com>
2023-05-19 13:33:19 +0200

    Downgrade source/taget compatibility

--- MacrobenchmarkSample/macrobenchmark/build.gradle.kts
+++ MacrobenchmarkSample/macrobenchmark/build.gradle.kts
@@ -20,11 +20,11 @@ android {
     targetProjectPath = ":app"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     testOptions.managedDevices.devices {
